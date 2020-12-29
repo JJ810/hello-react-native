ab688596be07a8f28ecd367ab6e61e2ab6c2bcb9
Bog <bog@Bogs-Mac.local>
2020-12-29 07:56:06 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type VersionInfo = {
+  version: {
+    major: number;
+    minor: number;
+    patch: number;
+  };
+};
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    const useHermes = global.hasOwnProperty('HermesInternal');
+    const RNVersion: VersionInfo = require('../node_modules/react-native/Libraries/Core/ReactNativeVersion');
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native {RNVersion.version.minor}
+        </Text>
+        <Text>
+          {useHermes
+            ? 'JavaScript engine: Hermes'
+            : 'JavaScript engine: JavaScriptCore'}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
